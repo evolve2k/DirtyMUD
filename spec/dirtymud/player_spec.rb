4aef53d877f757b146b83e97578f742db0029fc7
Gabe <gabe@avantbard.com>
2010-11-13 12:26:47 +0800

    add send_data to player

--- spec/dirtymud/player_spec.rb
+++ spec/dirtymud/player_spec.rb
@@ -62,5 +62,11 @@ describe Dirtymud::Player do
 
     end
 
+    describe '#send_data' do
+      it "delegates to the player connection object" do
+        @player.connection.should_receive(:send_data).with('foo')
+        @player.send_data('foo')
+      end
+    end
   end
 end
